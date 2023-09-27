f593398151b2a89230f153a796bd6be022276f0e
RVAISHU862003 <146222152+RVAISHU862003@users.noreply.github.com>
2023-09-27 21:36:48 +0530

    Add files via upload

---
+++ CHALLENGE  UNIT 2/challenge 2.2.py
@@ -0,0 +1,37 @@
+# Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality. 
+
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        return self.__account_balance
+
+
+# Testing the BankAccount class
+bank_account = BankAccount("123456789", "John Doe", 1000)
+print("Initial balance:", bank_account.display_balance())
+
+# Deposit money
+bank_account.deposit(500)
+print("Balance after deposit:", bank_account.display_balance())
+
+# Withdraw money
+bank_account.withdraw(200)
+print("Balance after withdrawal:", bank_account.display_balance())
